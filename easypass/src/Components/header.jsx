91ffb5f9209547d719dbd6c87bc15f2eddf4ee9a
AlexandreOliveira257 <alexandrept59@gmail.com>
2025-11-11 10:02:22 +0000

    Alteração do background css

--- easypass/src/Components/header.jsx
+++ easypass/src/Components/header.jsx
@@ -1,8 +1,8 @@
 import "../Header.css"
 function Header(){
     return <header >
-        <img className="bus" src="/EasyPass.png"/>
-        <img className="avatar" src="/Generic avatar.png"/>
+        <img className="bus" src="EasyPass.png"/>
+        <img className="avatar" src="Generic-avatar.png"/>
         </header> 
 }
 export default Header
