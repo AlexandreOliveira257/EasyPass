97cd8fbe0e3b6d27bcc286aac71cfdec47c5168b
AlexandreOliveira257 <alexandrept59@gmail.com>
2025-11-12 18:27:47 +0000

    Pequenos ajustes na visualização de imagens

--- easypass/src/Components/menu.jsx
+++ easypass/src/Components/menu.jsx
@@ -3,15 +3,15 @@ function Menu(){
     return <div className="barraMenu">
             <h1>Menu</h1>
             <nav className="iconsMenu">
-                <a href="#"><img src="icons/passe.svg" alt="Passes" />Passes</a>
-                <a href="#"><img src="icons/pedidos.svg" alt="Passes" />Pedidos</a>
-                <a href="#"><img src="icons/movimentos.svg" alt="Passes" />Movimentos</a>
-                <a href="#"><img src="icons/user.svg" alt="Passes" />Perfil</a>
-                <a href="#"><img src="icons/horários.svg" alt="Passes" />Horários</a>
-                <a href="#"><img src="icons/localizar.svg" alt="Passes" />Localizar Transporte</a>
+                <a href="#"><img src="icons/passe.svg"/>Passes</a>
+                <a href="#"><img src="icons/pedidos.svg"/>Pedidos</a>
+                <a href="#"><img src="icons/movimentos.svg"/>Movimentos</a>
+                <a href="#"><img src="icons/User.svg"/>Perfil</a>
+                <a href="#"><img src="icons/horários.svg"/>Horários</a>
+                <a href="#"><img src="icons/localizar.svg"/>Localizar Transporte</a>
             <div className="barra"></div>
-                <a href="#" className="definicoes"><img src="icons/settings.svg" alt="Passes" />Definições</a>
-                <a href="#" className="terminarSessao"><img src="icons/logout.svg" alt="Passes" />Terminar Sessão</a>
+                <a href="#" className="definicoes"><img src="icons/settings.svg" />Definições</a>
+                <a href="#" className="terminarSessao"><img src="icons/logout.svg" />Terminar Sessão</a>
             </nav>
         </div> 
 }
