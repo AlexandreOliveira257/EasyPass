68bdc5b9ccef7afc72619cd19adaa11b25356985
AlexandreOliveira257 <alexandrept59@gmail.com>
2025-12-17 15:37:50 +0000

    Backend do login a ser começado

---
+++ backend/login.php
@@ -0,0 +1,17 @@
+<?php
+function estabelerConexao()
+{
+    $host = 'ftp.antrob.eu';
+    $db   = 'u506280443_migaleDB';
+    $user = 'u506280443_migaledbUser';
+    $pass = '+9w6?HZu';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    return $pdo = new PDO($dsn, $user, $pass, $options);
+}
