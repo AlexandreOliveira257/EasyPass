1a7f0aea0cfac1465d43d05615aa3c62a3669bff
AlexandreOliveira257 <alexandrept59@gmail.com>
2025-12-27 23:49:11 +0000

    Tentiva de correção dos headers do php

--- backend/pagamento.php
+++ backend/pagamento.php
@@ -1,8 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 include "DBConnection.php";
 
 $input = file_get_contents("php://input");
