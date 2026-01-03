915dfa5bf57a69958ae15afd235008b48f8cc005
AlexandreOliveira257 <alexandrept59@gmail.com>
2026-01-03 16:39:22 +0000

    CSS adicinado a pedidos e movimentos

--- frontend/src/Components/movimentosMenuContent.jsx
+++ frontend/src/Components/movimentosMenuContent.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import PortalMenu from "./portalMenu";
 import { useUser } from "../Contexts/UserContext";
-
+import "../Movimentos.css"
 function MovimentosMenuContent(){
     const {t} = useTranslation()
     const {movimentos} = useUser()
@@ -10,18 +10,18 @@ function MovimentosMenuContent(){
         <h1>{t('movimentos').toUpperCase()}</h1>
         <div className="hrPortal"></div>
         {movimentos.length >= 1 ? (
-          movimentos.map((el) => (
-            <div className="pedidoContainer">
-            <div className="align">
-            <span>{el.descricao}</span>
-            <span>{el.nome_tipo}</span>
-            <span>{el.data_hora}</span>
-            <span>{el.valor}</span>
-            <span>{el.saldo_anterior}</span>
-            <span>{el.saldo_posterior}</span>
-            </div>
-            </div>
-          ))
+  movimentos.map((el) => (
+    <div className="movimentos-container" key={el.id}>
+      <div className="movimentos-row">
+        <div className="descricao">{el.descricao}</div>
+        <div className="tipo">{el.nome_tipo}</div>
+        <div className="data">{el.data_hora}</div>
+        <div className="valor">{el.valor}</div>
+        <div className="saldo-anterior">{el.saldo_anterior}</div>
+        <div className="saldo-posterior">{el.saldo_posterior}</div>
+      </div>
+    </div>
+  ))
         ) : ( <p>Não existem movimentos</p> )}
     </PortalMenu>
     )
--- frontend/src/Components/pedidosMenuContent.jsx
+++ frontend/src/Components/pedidosMenuContent.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router"
 import { useTranslation } from "react-i18next"
 import { useState } from "react"
 import { useUser } from "../Contexts/UserContext"
-
+import "../Pedidos.css"
 function PedidosMenuContent(){
   const [showPedido,setShowPedido] = useState(false);
   const {pedidos} = useUser();
@@ -16,12 +16,14 @@ function PedidosMenuContent(){
         <div className="hrPortal"></div>
         {pedidos.length >= 1 ? (
           pedidos.map((el) => (
-            <div className="pedidoContainer">
-            <img src="EasyPass.png" width={50}></img>
-            <div className="align">
+            <div className="pedido-container">
+              <div className="pedido-image-container">
+                <img src="EasyPass.png"></img>
+              </div>
+            <div className="pedido-container-mensagem">
             <span>{el.mensagem}</span>
-            <span>{el.estado_pedido_descricao}</span>
             <span>{el.data_emissao}</span>
+            <span>{el.estado_pedido_descricao}</span>
             </div>
             </div>
           ))
